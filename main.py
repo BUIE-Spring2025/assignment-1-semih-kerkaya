8554a035d4ac385491a7a2054d57012feca07112
semih-kerkaya <semih.kerkaya@std.bogazici.edu.tr>
2025-03-01 13:59:38 +0300

    attempt-3

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 def int_to_roman(num):
-    if num >= 4000:
-        print("Please enter a number less than 4000")
-    if num <= 0:
-        print("Please enter a positive number")
-    if type(num) != int:
-        print("Please enter an integer")
+    if type(num) != int or num >= 4000 or num <= 0:
+        return "Please enter an integer between 1 and 3999"
     else:
         roman_num = ""
         units_digit = num % 10
@@ -25,29 +21,29 @@ def int_to_roman(num):
         if hundreds_digit == 9:
             roman_num += "CM"
         if hundreds_digit <= 3:
-            roman_num = "C" * hundreds_digit
+            roman_num += "C" * hundreds_digit
         if hundreds_digit >= 5 and hundreds_digit <= 8:
-            roman_num = "D" + ("C" * hundreds_digit)
+            roman_num += "D" + ("C" * (hundreds_digit-5))
 
         if tens_digit == 4:
             roman_num += "XL"
         if tens_digit == 9:
             roman_num += "XC"
         if tens_digit <= 3:
-            roman_num = "X" * tens_digit
+            roman_num += "X" * tens_digit
         if tens_digit >= 5 and tens_digit <= 8:
-            roman_num = "L" + ("X" * tens_digit)
+            roman_num += "L" + ("X" * (tens_digit-5))
 
         if units_digit == 4:
             roman_num += "IV"
         if units_digit == 9:
             roman_num += "IX"
         if units_digit <= 3:
-            roman_num = "I" * units_digit
+            roman_num += "I" * units_digit
         if units_digit >= 5 and units_digit <= 8:
-            roman_num = "V" + ("I" * units_digit)
+            roman_num += "V" + ("I" * (units_digit-5))
         return roman_num
 
 
 
-print(int_to_roman(3999))
+print(int_to_roman(500))
